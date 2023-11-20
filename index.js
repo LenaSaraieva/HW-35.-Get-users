f19acf03b4c957e8f2e97fa4b6a7b1814a95cac8
LenaSaraieva <lena.saraeva1904@gmail.com>
2023-11-20 15:03:02 +0200

    js branch1

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-function makeRequest (method, path, callback) {
+function makeRequest(method, path, callback) {
     let xml = new XMLHttpRequest();
 
     xml.addEventListener('readystatechange', function () {
         if (xml.readyState === 4) {
             if (xml.status === 200) {
                 let data = JSON.parse(xml.responseText);
+                callback(null, data); 
+            } else {
                 callback('Failed request', null);
             }
         }
     });
+
     xml.open(method, path, true);
     xml.send();
 }
 
+const requestMethod = 'GET';
+const requestPath = 'path/to/data.json'; 
 
-const requetMethod = 'GET';
-const requetPath = 'patn/to/data.json';
-
-function dataCallback (error, data) {
+function dataCallback(error, data) {
     if (error) {
         console.error(error);
     } else {
-       console.log("Data:", data)
+        console.log("Data:", data);
     }
 }
+
 makeRequest(requestMethod, requestPath, dataCallback);
